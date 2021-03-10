714e857081a1086736ddfaece8178a9b808b1e01
waelabohamza <waeleagle1243@gmail.com>
2021-03-10 23:17:14 +0200

    refresh

--- lib/pages/home/homescreen.dart
+++ lib/pages/home/homescreen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:moj/component/crud.dart';
 import 'package:moj/const.dart';
@@ -18,10 +20,13 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   Crud crud = new Crud();
 
   bool isLoading = true;
+
+  PageController pageController;
 
   List listHomeDataServicesFavorite = [];
   List listHomeDataServicesCommon = [];
@@ -57,12 +62,28 @@ class _HomeScreenState extends State<HomeScreen> {
         isLoading = false;
       });
     }
+    startAnimate();
   }
 
   @override
   void initState() {
+    pageController = PageController(viewportFraction: 0.8);
+
     _getData();
     super.initState();
+  }
+
+  startAnimate() {
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      int nextPage = pageController.page.round() + 1;
+      if (nextPage == listHomeDataCategories.length) {
+        nextPage = 0;
+      }
+      pageController
+          .animateToPage(nextPage,
+              duration: Duration(seconds: 1), curve: Curves.linear)
+          .then((_) => startAnimate());
+    });
   }
 
   @override
@@ -190,9 +211,94 @@ class _HomeScreenState extends State<HomeScreen> {
                     "القضايا",
                     style: TextStyle(fontSize: 16, color: Colors.black),
                   )),
-              ListBottom(mdw: mdw, list: listHomeDataCategories),
+              // ListBottom(mdw: mdw, list: listHomeDataCategories),
+
+              Container(
+                height: 150,
+                child: PageView.builder(
+                    controller: pageController,
+                    itemCount: listHomeDataCategories.length,
+                    itemBuilder: (context, i) {
+                      return carousal(listHomeDataCategories, i, mdw);
+                    }),
+              ),
+
               SizedBox(height: 30),
             ],
           );
+  }
+
+  Widget carousal(list, i, mdw) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return Services(
+              index: i, categories: list, catid: list[i]['categories_id']);
+        }));
+      },
+      child: Container(
+        // padding: EdgeInsets.symmetric(horizontal: mdw / 14),
+        child: Card(
+          elevation: 3,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          "${list[i]['categories_name_ar']}",
+                          style: TextStyle(fontSize: 16, color: mainColor),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "${list[i]['categories_desc_ar']}",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12, color: Colors.black),
+                        ),
+                      ),
+                      MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        color: mainColor,
+                        textColor: Colors.white,
+                        onPressed: () {},
+                        child: Text(
+                          "ابدا الخدمة",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // SizedBox(width: 3),
+                // Expanded(
+                //     child: Column(
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   children: [
+                //     RaisedButton(
+                //       color: mainColor,
+                //       textColor: Colors.white,
+                //       onPressed: () {},
+                //       child: Text(
+                //         "ابدا الخدمة",
+                //         style: TextStyle(fontSize: 12),
+                //       ),
+                //     ),
+                //   ],
+                // ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
